c4c6e8509c1da3fb8e3d98f01705c78774c415d3
Козлов Сергей Викторович <sergeykozlov.2011@yandex.ru>
2020-01-04 16:16:16 +0300

    прототип нового движка для форм

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import Input from "./ui/components/Input";
+import { changeField } from "./core/features/formSlice";
+import { useDispatch, useSelector } from "react-redux";
+import get from "lodash/get";
+
+const idContext = React.createContext<any>(null);
+
+const Form = ({ children, id }: any) => {
+  const Provider = idContext.Provider;
+  const value = useSelector(state => get(state, `form.${id}.values`));
+  return <Provider value={{ id }}>{children(value)}</Provider>;
+};
+
+const Field = ({ component: Component, ...rest }: any) => {
+  const ctx = useContext(idContext);
+  const valueComponent = useSelector(state => {
+    return get(state, `form.${ctx.id}.values.${rest.name}`);
+  });
+  const dispatch = useDispatch();
+  const handleChange = (value: any) => {
+    dispatch(changeField({ ...value, id: ctx.id }));
+  };
+  return (
+    <Component
+      {...rest}
+      initialValue={rest.value}
+      onChange={handleChange}
+      value={valueComponent}
+    />
+  );
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form id={"test"}>
+        {(value: any) => {
+          return (
+            <>
+              <Field
+                component={Input}
+                name={"name"}
+                disabled={false}
+                required={true}
+                label={"Name"}
+              />
+              <Field
+                component={Input}
+                name={"lastName"}
+                disabled={false}
+                required={false}
+                label={"LastName"}
+              />
+            </>
+          );
+        }}
+      </Form>
     </div>
   );
-}
+};
 
 export default App;
